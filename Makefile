10d891cf4e72163afe12bc5a93c532cc7ef9c591
Boris Randebrock <brandebr@car6s2.42barcelona.com>
2023-11-08 19:57:53 +0100

    thanks for all the fish printf

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: brandebr <brandebr@student.42barcelona.co  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/08 13:20:26 by brandebr          #+#    #+#              #
-#    Updated: 2023/11/08 18:30:54 by brandebr         ###   ########.fr        #
+#    Updated: 2023/11/08 19:56:32 by brandebr         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,12 +18,12 @@ OBJ = $(SRC:%.c=%.o)
 
 CC = cc
 
-FLAGS = -Wall -Wextra -Werror -g -I. -I./libft -I.ft_printf
+FLAGS = -Wall -Wextra -Werror -g -I. -I./libft -I./ft_printf
 
 LIBFT = ./Libft/libft.a
 LIBFT_DIR = ./libft
 
-FT_PRINTF = ./ft_printf/ft_printf.a
+FT_PRINTF = ./ft_printf/libftprintf.a
 FT_PRINTF_DIR = ./ft_printf
 
 MLX = ./mlx/libmlx.a
@@ -31,11 +31,11 @@ MLX_DIR = ./mlx
 
 all: $(NAME) Makefile
 
-%.o: %.c so_long.h ./Libft/libft.h ./ft_printf.h Makefile
+%.o: %.c so_long.h ./Libft/libft.h ./ft_printf/libftprintf.h Makefile
 	$(CC) $(FLAGS) -c $< -o $@
 
 $(NAME): $(OBJ) $(LIBFT) $(FT_PRINTF) $(MLX)
-	$(CC) $(FLAGS) $(OBJ) $(LIBFT) $(PRINTF) $(MLX) -o $(NAME)
+	$(CC) $(FLAGS) $(OBJ) $(LIBFT) $(FT_PRINTF) $(MLX) -o $(NAME)
 
 $(FT_PRINTF):
 	@make all -C $(FT_PRINTF_DIR)
--- so_long.h
+++ so_long.h
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brandebr <brandebr@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 17:26:44 by brandebr          #+#    #+#             */
-/*   Updated: 2023/11/08 19:49:53 by brandebr         ###   ########.fr       */
+/*   Created: 2023/11/08 19:17:48 by brandebr          #+#    #+#             */
+/*   Updated: 2023/11/08 19:28:38 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
 
-int	main(void)
-{
-		write(1, "Hello Sasha", 11);
-		ft_printf(" 🐬 thanks for all  the fish 🖖");
-}
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include "Libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include "mlx/mlx.h"
+
+#endif
