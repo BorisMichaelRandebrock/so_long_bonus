bd6fb5defb0c24aa1ad48b8317082dff0d80bb48
Boris Randebrock <brandebr@car9s2.42barcelona.com>
2024-01-05 10:54:18 +0100

    last fine tunings..

--- Libft/ft_file_to_dptr.c
+++ Libft/ft_file_to_dptr.c
@@ -6,11 +6,43 @@
 /*   By: brandebr <brandebr@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 14:22:19 by brandebr          #+#    #+#             */
-/*   Updated: 2024/01/04 16:50:51 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/01/05 10:37:15 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+char **ft_file_to_dptr_m(int fd)
+{
+    char	**lines;
+	char	*line;
+    size_t	line_count;
+    char	**new_lines;
+
+    lines = NULL;
+    line_count = 0;
+    while (42)
+    {
+        line = get_next_line(fd);
+        if (line == NULL)
+            break;
+        new_lines = malloc((line_count + 2) * sizeof(char *));
+        if (new_lines == NULL)
+            return NULL;
+        size_t i = 0;
+        while (i < line_count)
+        {
+            new_lines[i] = lines[i];
+            i++;
+        }
+        new_lines[line_count] = line;
+        new_lines[line_count + 1] = NULL;
+        free(lines);
+        lines = new_lines;
+        line_count++;
+    }
+    return lines;
+}
+/*
 char	**ft_file_to_dptr_m(int fd)
 {
 	char	**lines;
@@ -31,4 +63,4 @@ char	**ft_file_to_dptr_m(int fd)
 	lines = realloc(lines, (line_count + 1) * sizeof(char *));
 	lines[line_count] = NULL;
 	return (lines);
-}
+}*/
